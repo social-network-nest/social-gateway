7b7985bb0e4cb2bf0a6c283dfeea232c1c2aab31
YTheSumsito <marcelo.jara@gmail.com>
2025-04-18 03:31:09 -0400

    send payload post-ms

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get, Inject, Post } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, Payload } from '@nestjs/microservices';
 
 @Controller('post')
 export class PostController {
   constructor(@Inject('POST_SERVICE') private readonly client: ClientProxy) {}
 
   @Post()
-  async create() {
-    return this.client.send({ cmd: 'create_post' }, {});
+  async create(@Payload() payload: any) {
+    return this.client.send({ cmd: 'create_post' }, payload);
   }
 }
