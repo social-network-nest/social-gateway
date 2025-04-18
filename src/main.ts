020119effb3db4537ba14a146adb6162de866d35
YTheSumsito <marcelo.jara@gmail.com>
2025-04-17 23:46:29 -0400

    add logger port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Social-Gateway');
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  logger.log('Social-Gateway running on port 3000');
 }
 bootstrap();
