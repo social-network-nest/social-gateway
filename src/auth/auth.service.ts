0a68145fb55f68a9c92ceca49aff658b7ea5f46a
YTheSumsito <marcelo.jara@gmail.com>
2025-04-21 23:47:13 -0400

    add auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @Inject('AUTH_SERVICE') private readonly authClient: ClientProxy
+    ) {}
+
+    async validateToken(token: string) {
+        const response = await this.authClient
+            .send({ cmd: 'verify.token' }, token)
+            .toPromise();
+
+        if (!response?.valid || !response.payload) {
+            throw new UnauthorizedException('Token is invalid or expired');
+        }
+
+        return response.payload;
+    }
+}
